f7c52233c39b7cdf9894457283a95289ed7f7c7e
Willi Krappen <wkrappen91@yahoo.de>
2016-12-03 01:54:25 +0100

    add blank route

---
+++ routes/Blank/Blank.js
@@ -0,0 +1,37 @@
+/**
+ * React App SDK (https://github.com/kriasoft/react-app)
+ *
+ * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import Layout from '../../components/Layout';
+import Link from '../../components/Link';
+import s from './Blank.css';
+
+const title = 'React App Starter Kit';
+
+class Blank extends React.Component {
+
+  static propTypes = {
+    articles: PropTypes.array.isRequired,
+  };
+
+  componentDidMount() {
+    document.title = title;
+  }
+
+  render() {
+    return (
+      <Layout className={s.content}>        
+       Blank
+      </Layout>
+    );
+  }
+
+}
+
+export default Blank;
